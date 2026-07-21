c27d9985b18d9a8225ed7bbb9e19a2dc2881f008
Clément Nunes <nunes.clement19@gmail.com>
2026-07-21 11:55:22 -0500

    fix

--- my-amazing-app-name/main.py
+++ my-amazing-app-name/main.py
@@ -1,6 +1,7 @@
+import numpy as np
+
 from waggle.plugin import Plugin
 from waggle.data.vision import Camera
-import numpy as np
 
 
 def compute_mean_color(image):
@@ -8,23 +9,22 @@ def compute_mean_color(image):
 
 
 def main():
-    input = "example.jpg"
-
-    with Plugin() as plugin, Camera(input) as camera:
-        snapshot = camera.snapshot()
+    with Plugin() as plugin:
+        # open camera and take snapshot
+        with Camera("left") as camera:
+            snapshot = camera.snapshot()
 
         # compute mean color
         mean_color = compute_mean_color(snapshot.data)
 
-        # print mean color
-        print(mean_color)
-
-        # publish color
+        # publish mean color
         plugin.publish("color.mean.r", mean_color[0], timestamp=snapshot.timestamp)
         plugin.publish("color.mean.g", mean_color[1], timestamp=snapshot.timestamp)
         plugin.publish("color.mean.b", mean_color[2], timestamp=snapshot.timestamp)
 
-
+        # save and upload image
+        snapshot.save("snapshot.jpg")
+        plugin.upload_file("snapshot.jpg", timestamp=snapshot.timestamp)
 
 
 if __name__ == "__main__":
